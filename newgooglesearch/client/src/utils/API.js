e3609eedb0fe4bb81da8ed288f7201b28b497552
Griffin <griffincriste@gmail.com>
2019-09-16 12:57:25 -0500

    basic setup

---
+++ newgooglesearch/client/src/utils/API.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=";
+const APIKEY = "AIzaSyBWgk7NCKjutLOsQ4kTytTIF6DRkvU4vs0";
+
+
+
+// GET https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
+// GET https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
+
+export default {
+  getBooks: function(query) {
+    return axios.get(BASEURL + query + APIKEY);
+  },
+  // Gets all books
+  // getBooks: function() {
+  //   return axios.get("/api/books");
+  // },
+  // Gets the book with the given id
+  getBook: function(id) {
+    return axios.get("/api/books/" + id);
+  },
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+  },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
+  }
+};
